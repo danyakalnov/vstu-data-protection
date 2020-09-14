d61ab5385fc78ad7e01bccf21736fce6a2a8ba34
Danila Kalnov <danyakalnov@gmail.com>
2020-09-14 20:33:10 +0400

    Add english alphabet and another special characters

--- autokeyCipher.js
+++ autokeyCipher.js
@@ -1,44 +1,67 @@
-const alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю', 'Я'];
-const alphabetPower = alphabet.length;
+const alphabet = []
+
+for (let i = 32; i <= 126; i++) {
+  alphabet.push(String.fromCharCode(i))
+}
+
+for (let i = 1040; i <= 1103; i++) {
+  alphabet.push(String.fromCharCode(i))
+}
+
+const alphabetPower = alphabet.length
 
 const cipher = (text, key) => {
-    const autokey = text.slice(0, -key.length);
-    const resultKey = key.concat(autokey);
-    let cipherText = '';
-  
-    const encryptSymbol = (textSymbol, keySymbol) => {
-      let encryptedSymbolIndex = (alphabet.indexOf(keySymbol) + alphabet.indexOf(textSymbol)) % alphabetPower;
-      let encryptedSymbol = alphabet[encryptedSymbolIndex];
-  
-      return encryptedSymbol;
+  const autokey = text.slice(0, -key.length)
+  const resultKey = key.concat(autokey)
+  let cipherText = ''
+
+  const encryptSymbol = (textSymbol, keySymbol) => {
+    let encryptedSymbolIndex =
+      (alphabet.indexOf(keySymbol) + alphabet.indexOf(textSymbol)) % alphabetPower
+    let encryptedSymbol = alphabet[encryptedSymbolIndex]
+    if (!encryptedSymbol) {
+      console.log(
+        alphabet.indexOf(keySymbol),
+        alphabet.indexOf(textSymbol),
+        'Рез-тат: ',
+        encryptedSymbolIndex,
+      )
     }
-  
-    for (let i = 0; i < text.length; i++) {
-      let encryptedSymbol = encryptSymbol(text[i], resultKey[i]);
-      cipherText += encryptedSymbol;
-    }
-  
-    return [cipherText, autokey];
+
+    return encryptedSymbol
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    let encryptedSymbol = encryptSymbol(text[i], resultKey[i])
+    cipherText += encryptedSymbol
+  }
+
+  return [cipherText, autokey]
 }
 
 const decipher = (encryptedText, key) => {
-  let decryptedText = '';
-  let currentKey = key;
+  let decryptedText = ''
+  let currentKey = key
 
   const decryptSymbol = (textSymbol, keySymbol) => {
-    let decryptedSymbolIndex = (alphabet.indexOf(textSymbol) - alphabet.indexOf(keySymbol)) % alphabetPower;
-    if (decryptedSymbolIndex < 0) decryptedSymbolIndex += alphabetPower;
-    let decryptedSymbol = alphabet[decryptedSymbolIndex];
+    let decryptedSymbolIndex =
+      (alphabet.indexOf(textSymbol) - alphabet.indexOf(keySymbol)) % alphabetPower
+    if (decryptedSymbolIndex < 0) decryptedSymbolIndex += alphabetPower
+    let decryptedSymbol = alphabet[decryptedSymbolIndex]
 
-    return decryptedSymbol;
+    return decryptedSymbol
   }
 
   for (let i = 0; i < encryptedText.length; i++) {
-    let decryptedSymbol = decryptSymbol(text[i], key[i]);
-    decryptedText += decryptedSymbol;
-    currentKey += decryptedSymbol;
+    let decryptedSymbol = decryptSymbol(encryptedText[i], currentKey[i])
+    decryptedText += decryptedSymbol
+    currentKey += decryptedSymbol
   }
 
-  return decryptedText;
+  return decryptedText
 }
 
+module.exports = {
+  cipher,
+  decipher,
+}
